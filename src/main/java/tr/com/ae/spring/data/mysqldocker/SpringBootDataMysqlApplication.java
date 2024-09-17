a7108759be59da70ebabe7de92e1c0c563c1ac53
ahmeterdem <ahmet.erdem@gmail.com>
2024-09-17 15:17:00 +0300

    Ana uygulma ve test sınıfı eklendi.

---
+++ src/main/java/tr/com/ae/spring/data/mysqldocker/SpringBootDataMysqlApplication.java
@@ -0,0 +1,16 @@
+package tr.com.ae.spring.data.mysqldocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Prepared by ahmeterdem on 17.09.2024 for Project app_mysqlfromdocker.
+ */
+@SpringBootApplication
+public class SpringBootDataMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tr/com/ae/spring/data/mysqldocker/SpringBootDataMysqlApplicationTests.java
@@ -0,0 +1,16 @@
+package tr.com.ae.spring.data.mysqldocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Prepared by ahmeterdem on 17.09.2024 for Project app_mysqlfromdocker.
+ */
+@SpringBootTest
+class SpringBootDataMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
